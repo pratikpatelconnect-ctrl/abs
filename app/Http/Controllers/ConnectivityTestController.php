e5ecb5d042c8f720cb8cfa7f605fabc4d875689d
pratik <pratik@sivreninnovations.com>
2025-09-12 13:46:02 +0530

    jkljkljkljkljk

--- app/Http/Controllers/ConnectivityTestController.php
+++ app/Http/Controllers/ConnectivityTestController.php
@@ -39,6 +39,35 @@ class ConnectivityTestController extends Controller
         $request_body = [
             'message' => 'Connectivity Test',
         ];
+        
+        // Debug: Check certificate files exist
+        $certPath = storage_path('app/certs/uobuat_sivren_org.crt');
+        $keyPath = storage_path('app/certs/uobuat_sivren_org.pem');
+        
+        if (!file_exists($certPath)) {
+            return response()->json([
+                'message' => 'Certificate file not found',
+                'cert_path' => $certPath,
+            ], 500);
+        }
+        
+        if (!file_exists($keyPath)) {
+            return response()->json([
+                'message' => 'Private key file not found',
+                'key_path' => $keyPath,
+            ], 500);
+        }
+        
+        // Debug: Log the configuration being used
+        \Log::info('Connectivity Test Configuration', [
+            'client_slug' => $clientSlug,
+            'client_config' => $clientConfig,
+            'url' => $url,
+            'headers' => $header,
+            'cert_exists' => file_exists($certPath),
+            'key_exists' => file_exists($keyPath),
+        ]);
+        
         // Initialize cURL
         $ch = curl_init();
         
@@ -50,15 +79,19 @@ class ConnectivityTestController extends Controller
             CURLOPT_POSTFIELDS => json_encode($request_body),
             CURLOPT_HTTPHEADER => [
                 'Content-Type: application/json',
+                'Accept: application/json',
                 'clientID: ' . $header['clientID'],
                 'requestID: ' . $header['requestID'],
                 'x-api-key: ' . $header['x-api-key'],
                 'aggregatorKeyAlias: ' . $header['aggregatorKeyAlias'],
             ],
-            CURLOPT_SSLCERT => storage_path('app/certs/uobuat_sivren_org.crt'),
-            CURLOPT_SSLKEY => storage_path('app/certs/uobuat_sivren_org.pem'),
+            CURLOPT_SSLCERT => $certPath,
+            CURLOPT_SSLKEY => $keyPath,
+            CURLOPT_SSLCERTTYPE => 'PEM',
+            CURLOPT_SSLKEYTYPE => 'PEM',
             CURLOPT_SSL_VERIFYPEER => false, // Set to true and provide CA cert if needed
             CURLOPT_SSL_VERIFYHOST => 2,
+            CURLOPT_SSLVERSION => CURL_SSLVERSION_TLSv1_2,
             CURLOPT_TIMEOUT => 30,
             CURLOPT_CONNECTTIMEOUT => 10,
             CURLOPT_FOLLOWLOCATION => true,
@@ -69,9 +102,21 @@ class ConnectivityTestController extends Controller
         $response = curl_exec($ch);
         $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         $error = curl_error($ch);
+        $curlInfo = curl_getinfo($ch);
         
         // Close cURL handle
         curl_close($ch);
+        
+        // Debug logging
+        \Log::info('Connectivity Test Request Debug', [
+            'url' => $url,
+            'headers' => $header,
+            'request_body' => $request_body,
+            'http_code' => $httpCode,
+            'curl_error' => $error,
+            'curl_info' => $curlInfo,
+            'response' => $response,
+        ]);
         
         // Check for cURL errors
         if ($response === false || !empty($error)) {
@@ -83,6 +128,7 @@ class ConnectivityTestController extends Controller
                 ],
                 'message' => 'cURL request failed',
                 'error' => $error ?: 'Unknown cURL error',
+                'curl_info' => $curlInfo,
             ], 502);
         }
         
@@ -97,6 +143,7 @@ class ConnectivityTestController extends Controller
                 'message' => 'Upstream request failed',
                 'error' => $response,
                 'http_code' => $httpCode,
+                'curl_info' => $curlInfo,
             ], $httpCode);
         }
         
