99b9cb4e71fe0678753df9d4cdb6eb3f29f62064
pratik <pratik@sivreninnovations.com>
2025-11-21 09:33:31 +0530

    fdgdfgdfg

--- config/clients.php
+++ config/clients.php
@@ -53,4 +53,30 @@ return [
             ]
         ],
     ],
+    '96bmcredit' => [
+        'uat' => [
+            'client_id' => 'BO96B2532M0196B',
+            'x-api-key' => 'c11287ac-1ca3-445b-a9ae-1bae619fc987',
+            'pgp' => [
+                'issuer_id' => 'uobuat@sivren.com',
+                'passphrase' => 'uatuob2025',
+                'fingerprint' => '40044FCBD111BD3ADEBA4447BF7AC39F6DC6A2A2',
+                'issuer_key_id' => 'BF7AC39F6DC6A2A2',
+                'public_key' => storage_path('app/pgp/public.asc'),
+                'private_key' => storage_path('app/pgp/private.asc'),
+            ]
+        ],
+        'production' => [
+            'client_id' => '',
+            'x-api-key' => '',
+            'pgp' => [
+                'issuer_id' => '',
+                'passphrase' => '',
+                'fingerprint' => '',
+                'issuer_key_id' => '',
+                'public_key' => storage_path('app/pgp/public.asc'),
+                'private_key' => storage_path('app/pgp/private.asc'),
+            ]
+        ],
+    ],
 ];
